26ee35c7345032fc746c32209edd9109ab90a93c
Neimar Gustavo Lopes da Silva <neigustavo@gmail.com>
2018-10-03 01:31:19 +0000

    versao inicial

---
+++ src/main/java/br/com/yop/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package br.com.yop.repository;
+
+import br.com.yop.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
